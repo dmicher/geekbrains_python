fed88072efc430841a34e92c7c4af1415e67bdbb
Dmicher Abathur Kubrow <dscherkasov@yandex.ru>
2020-11-06 19:49:19 +0300

    Lesson 2, tasks 1-2 done

--- dmicher_python_app/dmicher_python_app/main_menu.py
+++ dmicher_python_app/dmicher_python_app/main_menu.py
@@ -1,4 +1,4 @@
-import lesson1.menu as l1
+import lesson1.menu as l1, lesson2.menu as l2
 
 # Выводит главное меню на экран
 def main_menu(show_hello):
@@ -8,6 +8,7 @@ def main_menu(show_hello):
         print()
     print("ОСНОВНОЕ МЕНЮ:")
     print("\t1\t= Урок 1: Знакомство с Python.")
+    print("\t2\t= Урок 2: Встроенные типы и операции с ними.")
     print()
     print("\tв, q\t= Выход\r\n")
 
@@ -19,6 +20,8 @@ while True:
         number = int(command)
         if number == 1:
             l1.show()
+        elif number == 2:
+            l2.show()
         else:
            continue
     elif command == "в" or command == "q" or command == "й" or command == "d":
